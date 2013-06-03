213bbbdf6420df3a28d1d6fe574e8acfad4bbd07
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2013-06-04 00:00:50 +0200

    Update RelationFinder.php

--- tutorials/relfinder_search/RelationFinder.php
+++ tutorials/relfinder_search/RelationFinder.php
@@ -1,10 +1,20 @@
 <?php
 
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <kurzum@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return Sebastian Hellmann
+ * ----------------------------------------------------------------------------
+ */
+
+
 //require_once('Filter.php');
 
 class RelationFinder {
 
-	// $endpointURI = "http://db0.aksw.org:8890";
+	// $endpointURI = "";
 	private $endpointURI = "http://dbpedia.org/sparql";
 	// default graphy URI can be empty, but it is usually fast to specify it
 	private $defaultGraphURI = "http://dbpedia.org";
